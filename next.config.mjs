dc3eeb5166db0d77d19187258877375e1698ce8e
Levi9111 <captainlevi9111@gmail.com>
2024-03-17 16:40:43 +0600

    next config file updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      bufferutil: 'commonjs bufferutil',
+      canvas: 'commonjs canvas',
+    });
+    // config.infrastructureLogging = { debug: /PackFileCache/ };
+    return config;
+  },
   images: {
     remotePatterns: [
       {
@@ -9,13 +18,8 @@ const nextConfig = {
       },
     ],
   },
-
-  webpack: (config) => {
-    config.externals.push({
-      sharp: 'commonjs sharp',
-      canvas: 'commonjs canvas',
-    });
-    return config;
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
