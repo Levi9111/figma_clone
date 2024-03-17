0ba034490094cd04e5fc97388c790e378b555108
Levi9111 <captainlevi9111@gmail.com>
2024-03-17 08:20:42 +0600

    right sidebar completed. Font size width family, color, stroke, Export to pdf button added

--- components/settings/Color.tsx
+++ components/settings/Color.tsx
@@ -1,4 +1,4 @@
-import { Label } from "../ui/label";
+import { Label } from '../ui/label';
 
 type Props = {
   inputRef: any;
@@ -14,25 +14,27 @@ const Color = ({
   placeholder,
   attributeType,
   handleInputChange,
-}: Props) => (
-  <div className='flex flex-col gap-3 border-b border-primary-grey-200 p-5'>
-    <h3 className='text-[10px] uppercase'>{placeholder}</h3>
-    <div
-      className='flex items-center gap-2 border border-primary-grey-200'
-      onClick={() => inputRef.current.click()}
-    >
-      <input
-        type='color'
-        value={attribute}
-        ref={inputRef}
-        onChange={(e) => handleInputChange(attributeType, e.target.value)}
-      />
-      <Label className='flex-1'>{attribute}</Label>
-      <Label className='flex h-6 w-8 items-center justify-center bg-primary-grey-100 text-[10px] leading-3'>
-        90%
-      </Label>
+}: Props) => {
+  return (
+    <div className='flex flex-col gap-3 border-b border-primary-grey-200 p-5'>
+      <h3 className='text-[10px] uppercase'>{placeholder}</h3>
+      <div
+        className='flex items-center gap-2 border border-primary-grey-200'
+        onClick={() => inputRef.current.click()}>
+        <input
+          type='color'
+          value={attribute}
+          ref={inputRef}
+          onChange={(e) => handleInputChange(attributeType, e.target.value)}
+        />
+        <Label className='flex-1'>{attribute}</Label>
+        {/* TODO: applay dynamic opacity  */}
+        {/* <Label className='flex h-6 w-8 items-center justify-center bg-primary-grey-100 text-[10px] leading-3'>
+          90%
+        </Label> */}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Color;
